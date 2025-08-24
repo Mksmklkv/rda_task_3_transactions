e02be1133d6ee1be02c7ebc32cb0a18e3e10b1fa
Maksym Kulakov <maksym.kulakov@ica.de>
2025-08-24 17:36:26 +0200

    Task done

--- task.sql
+++ task.sql
@@ -10,6 +10,6 @@ START TRANSACTION;
 -- And some data should be created inside the transaction 
 SET @order_id = LAST_INSERT_ID();
 INSERT INTO OrderItems (OrderID, ProductID, Count) values (@order_id, 1, 1);
-UPDATE Products set WarehouseAmount = WarehouseAmount - 1 where WarehouseAmount >= 1 and ID = 1;
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 where WarehouseAmount >= 1 and ID = 1;
 
 COMMIT; 
