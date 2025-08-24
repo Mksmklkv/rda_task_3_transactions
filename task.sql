21a4da2f5e19ab6b4d8725bac043cd16780df38b
Maksym Kulakov <maksym.kulakov@ica.de>
2025-08-24 17:32:27 +0200

    Task done

--- task.sql
+++ task.sql
@@ -2,12 +2,12 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders (CustomerID, Date) values (1, CURRENT_DATE); 
 
 -- Start the transaction 
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
-INSERT INTO Orders (CustomerID, Date) values (1, CURRENT_DATE); 
 SET @order_id = LAST_INSERT_ID();
 INSERT INTO OrderItems (OrderID, ProductID, Count) values (@order_id, 1, 1);
 UPDATE Products set WarehouseAmount = WarehouseAmount - 1 where WarehouseAmount >= 1 and ID = 1;
