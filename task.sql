511cdb692399b4e1034c3681400e074c6125a2fe
Maksym Kulakov <maksym.kulakov@ica.de>
2025-08-24 17:27:47 +0200

    Task done

--- task.sql
+++ task.sql
@@ -7,8 +7,9 @@ USE ShopDB;
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
-insert into Orders (CustomerID, Date) values (LAST_INSERT_ID(), CURRENT_DATE); 
-insert into OrderItems (OrderID, ProductID, Count) values (LAST_INSERT_ID(), 1, 1);
+insert into Orders (CustomerID, Date) values (1, CURRENT_DATE); 
+SET @order_id = LAST_INSERT_ID();
+insert into OrderItems (OrderID, ProductID, Count) values (@order_id, 1, 1);
 update Products set WarehouseAmount = WarehouseAmount - 1 where WarehouseAmount >= 1 and ID = 1;
 
 COMMIT; 
